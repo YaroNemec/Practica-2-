de600234106b46c07215ef567bcd1fcb5976e0d7
YaroNemec <62666356+YaroNemec@users.noreply.github.com>
2021-04-13 16:10:45 -0400

    P2-001: Agregé el GetStudent con su metodo return

--- Practica_2_WebApi/Controllers/StudentsController.cs
+++ Practica_2_WebApi/Controllers/StudentsController.cs
@@ -9,31 +9,17 @@ namespace Practica_2_WebApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class StudentsController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        public StudentsController()
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
+          
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public List<Student> GetStudent()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return new List<Student>();
         }
     }
 }
