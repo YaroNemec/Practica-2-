c702b2d8ad8756a84aef85e1de9a5917e693e940
YaroNemec <62666356+YaroNemec@users.noreply.github.com>
2021-04-13 16:01:07 -0400

    P2-001: Modifico el contenido de Student

--- Practica_2_WebApi/Student.cs
+++ Practica_2_WebApi/Student.cs
@@ -2,14 +2,8 @@ using System;
 
 namespace Practica_2_WebApi
 {
-    public class WeatherForecast
+    public class Student
     {
-        public DateTime Date { get; set; }
-
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string Summary { get; set; }
+        public String Student{get; set;}
     }
 }
